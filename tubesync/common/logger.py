512b70adadfbcc9c6408f8100072ed596389dce2
meeb <meeb@meeb.org>
2023-11-30 18:50:22 +1100

    toggle logging verbosity based on settings.DEBUG

--- tubesync/common/logger.py
+++ tubesync/common/logger.py
@@ -1,10 +1,14 @@
 import logging
+from django.conf import settings
+
+
+logging_level = logging.DEBUG if settings.DEBUG else logging.INFO
 
 
 log = logging.getLogger('tubesync')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging_level)
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging_level)
 formatter = logging.Formatter('%(asctime)s [%(name)s/%(levelname)s] %(message)s')
 ch.setFormatter(formatter)
 log.addHandler(ch)
