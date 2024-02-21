9e524666c25ff460831ce841aaa9e8932f35ca04
Re4zOon <9249934+Re4zOon@users.noreply.github.com>
2024-02-21 21:03:23 +0100

    Adding auth option for healthcheck

--- tubesync/healthcheck.py
+++ tubesync/healthcheck.py
@@ -12,24 +12,38 @@
 
 import sys
 import requests
-
+import os
 
 TIMEOUT = 5  # Seconds
 
-
-def do_heatlhcheck(url):
+def do_heatlhcheck(url, username, password):
     headers = {'User-Agent': 'healthcheck'}
-    response = requests.get(url, headers=headers, timeout=TIMEOUT)
+    if username and password:
+        response = requests.get(url, headers=headers, timeout=TIMEOUT, auth=(username, password))
+    else:
+        response = requests.get(url, headers=headers, timeout=TIMEOUT)
     return response.status_code == 200
+
+def get_http_auth():
+    if "HTTP_USER" in os.environ:
+        # Attempt to get the value of the environment variable
+        username = os.environ["HTTP_USER"]
+        password = os.environ["HTTP_PASS"]
+    else:
+        username = False 
+        password = False
+    return username, password
 
 
 if __name__ == '__main__':
+    username, password = get_http_auth()
     try:
         url = sys.argv[1]
     except IndexError:
         sys.stderr.write('URL must be supplied\n')
         sys.exit(1)
-    if do_heatlhcheck(url):
+
+    if do_heatlhcheck(url, username, password):
         sys.exit(0)
     else:
         sys.exit(1)
