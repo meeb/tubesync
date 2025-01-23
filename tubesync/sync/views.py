25892388a1316b67dd16e38cc1fd31c50651b1e6
tcely <tcely@users.noreply.github.com>
2025-01-22 20:42:18 -0500

    Remove the /channel/ from the URL

--- tubesync/sync/views.py
+++ tubesync/sync/views.py
@@ -297,11 +297,11 @@ class ValidateSourceView(FormView):
             '@' == self.key[0]
         )
         if use_channel_id:
-            source_type = self.source_type
             self.source_type_str = 'youtube-channel-id'
             self.source_type = self.source_types.get(self.source_type_str, None)
+            url = Source.create_index_url(self.source_type, self.key, 'videos')
             self.key = youtube.get_channel_id(
-                Source.create_index_url(source_type, self.key, 'videos')
+                url.replace('/channel/', '/')
             )
         for field in fields_to_populate:
             if field == 'source_type':
