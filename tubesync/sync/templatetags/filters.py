22cec876366721d70a9c0b5fcd33c4335e6f61b2
tcely <tcely@users.noreply.github.com>
2025-01-24 11:37:51 -0500

    Add fixB filter

--- tubesync/sync/templatetags/filters.py
+++ tubesync/sync/templatetags/filters.py
@@ -1,10 +1,20 @@
 from django import template
-from django.template.defaultfilters import filesizeformat
+from django.template.defaultfilters import filesizeformat, stringfilter
+
 
 register = template.Library()
 
+
+@register.filter(is_safe=True)
+@stringfilter
+def fixB(input):
+    return input.replace('B', 'iB')
+
+
 @register.filter(is_safe=True)
 def bytesformat(input):
-    intermediate = filesizeformat(input)
-    return intermediate.replace('B', 'iB')
+    output = filesizeformat(input)
+    if not (output and output.endswith('B')):
+        return output
+    return fixB(output)
 
