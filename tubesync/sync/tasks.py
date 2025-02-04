5cf7f1574ab5084a435ac13993bff2abfde39188
tcely <tcely@users.noreply.github.com>
2025-02-03 21:22:19 -0500

    We don't need the extra run

--- tubesync/sync/tasks.py
+++ tubesync/sync/tasks.py
@@ -349,7 +349,7 @@ def download_media_metadata(media_id):
                     priority=0,
                     queue=str(media.pk),
                     repeat=Task.HOURLY,
-                    repeat_until = published_datetime + timedelta(hours=2),
+                    repeat_until = published_datetime + timedelta(hours=1),
                     verbose_name=verbose_name.format(media.key, published_datetime.isoformat(' ', 'seconds')),
                     remove_existing_tasks=True,
                 )
