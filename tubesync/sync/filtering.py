806dde6cac71289bc33300e82e98ad9a27a663b2
tcely <tcely@users.noreply.github.com>
2025-02-14 15:01:55 -0500

    Do not allow filtering to turn off `Media.skip`

--- tubesync/sync/filtering.py
+++ tubesync/sync/filtering.py
@@ -12,7 +12,7 @@ from .overrides.custom_filter import filter_custom
 # Check the filter conditions for instance, return is if the Skip property has changed so we can do other things
 def filter_media(instance: Media):
     # Assume we aren't skipping it, if any of these conditions are true, we skip it
-    skip = instance.skip
+    skip = False
 
     # Check if it's published
     if not skip and filter_published(instance):
@@ -41,9 +41,9 @@ def filter_media(instance: Media):
 
     # Check if skipping
     if instance.skip != skip:
-        instance.skip = skip
+        instance.skip = instance.skip or skip
         log.info(
-            f"Media: {instance.source} / {instance} has changed skip setting to {skip}"
+            f"Media: {instance.source} / {instance} has changed skip setting to {instance.skip}"
         )
         return True
 
