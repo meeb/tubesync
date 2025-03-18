7e721c98a7ffbed8d4be12f3103b72220b4ee31c
tcely <tcely@users.noreply.github.com>
2025-03-18 17:42:06 -0400

    Don't update media servers for every Media item

--- tubesync/sync/signals.py
+++ tubesync/sync/signals.py
@@ -374,16 +374,3 @@ def media_post_delete(sender, instance, **kwargs):
             log.info(f'Deleting file for: {instance} path: {file}')
             delete_file(file)
 
-    if not instance.source.is_active:
-        return
-    # Schedule a task to update media servers
-    log.info(f'Scheduling media server updates')
-    verbose_name = _('Request media server rescan for "{}"')
-    for mediaserver in MediaServer.objects.all():
-        rescan_media_server(
-            str(mediaserver.pk),
-            priority=30,
-            verbose_name=verbose_name.format(mediaserver),
-            remove_existing_tasks=True,
-        )
-
