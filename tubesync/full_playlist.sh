3717e6d2744ace70d31de9c1f6f38e58b19081e8
tcely <tcely@users.noreply.github.com>
2025-03-30 13:23:46 -0400

    Update full_playlist.sh

--- tubesync/full_playlist.sh
+++ tubesync/full_playlist.sh
@@ -2,9 +2,17 @@
 
 echo "$1"
 echo "$2"
-exit 0
+playlist_id="${1}"
+total_entries="${2}"
+set -x
 
-if [ 'NA' != "$2" ] && [ "$1" != "$2" ]
+time find / -path '*/infojson/playlist/*' \
+    -name "postprocessor_[${playlist_id}]_*_${total_entries}_temp.info.json"
+
+exit 0
+downloaded_entries=0
+
+if [ 'NA' != "${total_entries}" ] && [ "${downloaded_entries}" != "${total_entries}" ]
 then
     exit 1
 fi
