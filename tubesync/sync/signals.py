4441f92193e7cbc595ccde79680ee0d5e01b8a66
tcely <tcely@users.noreply.github.com>
2025-04-12 06:23:56 -0400

    Fix indentation for signals.py

--- tubesync/sync/signals.py
+++ tubesync/sync/signals.py
@@ -9,12 +9,12 @@ from background_task.signals import task_failed
 from background_task.models import Task
 from common.logger import log
 from .models import Source, Media, MediaServer
-from .tasks import (delete_task_by_source, delete_task_by_media, index_source_task,
-                    download_media_thumbnail, download_media_metadata,
-                    map_task_to_instance, check_source_directory_exists,
-                    download_media, rescan_media_server, download_source_images,
-                    delete_all_media_for_source, save_all_media_for_source,
-                    rename_media, get_media_metadata_task, get_media_download_task)
+from .tasks import (  delete_task_by_source, delete_task_by_media, index_source_task,
+                      download_media_thumbnail, download_media_metadata,
+                      map_task_to_instance, check_source_directory_exists,
+                      download_media, rescan_media_server, download_source_images,
+                      delete_all_media_for_source, save_all_media_for_source,
+                      rename_media, get_media_metadata_task, get_media_download_task)
 from .utils import delete_file, glob_quote, mkdir_p
 from .filtering import filter_media
 from .choices import Val, YouTube_SourceType
