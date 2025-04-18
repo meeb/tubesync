c46d044c51c0d545ba1d87059313e6ef766143bf
tcely <tcely@users.noreply.github.com>
2025-04-17 22:02:37 -0400

    Add new models to admin.py

--- tubesync/sync/admin.py
+++ tubesync/sync/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Source, Media, MediaServer
+from .models import (
+    Source,
+    Media,
+    Metadata,
+    MetadataFormat,
+    MediaServer
+)
 
 
 @admin.register(Source)
@@ -19,6 +25,24 @@ class MediaAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'key', 'source', 'can_download', 'skip', 'downloaded')
     readonly_fields = ('uuid', 'created')
     search_fields = ('uuid', 'source__key', 'key')
+
+
+@admin.register(Metadata)
+class MetadataAdmin(admin.ModelAdmin):
+
+    ordering = ('-created',)
+    list_display = ('uuid', 'media', 'site', 'key', 'created', 'retrieved', 'uploaded', 'published')
+    readonly_fields = ('uuid', 'created')
+    search_fields = ('uuid', 'site', 'key')
+
+
+@admin.register(MetadataFormat)
+class MetadataFormatAdmin(admin.ModelAdmin):
+
+    ordering = ('site', 'key', 'number')
+    list_display = ('uuid', 'metadata', 'site', 'key', 'code')
+    readonly_fields = ('uuid', 'metadata')
+    search_fields = ('uuid', 'site', 'key', 'code')
 
 
 @admin.register(MediaServer)
