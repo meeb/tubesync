07f689e5d0d75edfa06242a687da6dc195df899a
tcely <tcely@users.noreply.github.com>
2025-05-04 00:04:00 -0400

    Update fix-mariadb.py

--- tubesync/sync/management/commands/fix-mariadb.py
+++ tubesync/sync/management/commands/fix-mariadb.py
@@ -1,13 +1,18 @@
 from django import db
 from pprint import pp
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy
 from django.core.management.base import BaseCommand, CommandError
 from common.logger import log
 
 
+def _(arg_str):
+    return str(gettext_lazy(arg_str))
+
 def SQLTable(arg_table):
-    assert isinstance(arg_table, str), type(arg_table)
-    assert arg_table.startswith('sync_'), _('Invalid table name')
+    if not isinstance(arg_table, str):
+        raise TypeError(type(arg_table))
+    if not arg_table.startswith('sync_'):
+        raise ValueError(_('Invalid table name'))
     return str(arg_table)
 
 
@@ -33,17 +38,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if 'mysql' != db.connection.vendor:
-            raise CommandError(_(
-                'An invalid database vendor is configured'
-                f': {db.connection.vendor}'
-            ))
+            raise CommandError(
+                _('An invalid database vendor is configured')
+                + f': {db.connection.vendor}'
+            )
         if not db.connection.mysql_is_mariadb():
             raise CommandError(_('Not conbected to a MariaDB database server.'))
 
-        uuid_columns = options.get('uuid-columns')
-        table_names = options.get('delete-table', list())
+        table_names = options.get('delete_table', list())
 
-        if options['uuid-columns']:
+        if options['uuid_columns']:
             self.stdout.write('Time to update the columns!')
 
         pp( table_names )
