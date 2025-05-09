0d9542163bf1d64f1b65b36886a071a645a911c3
tcely <tcely@users.noreply.github.com>
2025-05-08 23:43:16 -0400

    Update __init__.py

--- tubesync/sync/models/__init__.py
+++ tubesync/sync/models/__init__.py
@@ -36,25 +36,12 @@ from ..choices import (  Val, CapChoices, Fallback, FileExtension,
                         MediaState, SourceResolution, SourceResolutionInteger,
                         SponsorBlock_Category, YouTube_AudioCodec,
                         YouTube_SourceType, YouTube_VideoCodec)
+from .metadata import Metadata
 from .metadata_format import MetadataFormat
 from .media_server import MediaServer
 
 media_file_storage = FileSystemStorage(location=str(settings.DOWNLOAD_ROOT), base_url='/media-data/')
 _srctype_dict = lambda n: dict(zip( YouTube_SourceType.values, (n,) * len(YouTube_SourceType.values) ))
-
-class JSONEncoder(DjangoJSONEncoder):
-    item_separator = ','
-    key_separator = ':'
-
-    def default(self, obj):
-        try:
-            iterable = iter(obj)
-        except TypeError:
-            pass
-        else:
-            return list(iterable)
-        return super().default(obj)
-
 
 class Source(models.Model):
     '''
@@ -1784,150 +1771,4 @@ class Media(models.Model):
                             parent_dir = parent_dir.parent
                     except OSError as e:
                         pass
-
-
-class Metadata(models.Model):
-    '''
-        Metadata for an indexed `Media` item.
-    '''
-    class Meta:
-        db_table = 'sync_media_metadata'
-        verbose_name = _('Metadata about Media')
-        verbose_name_plural = _('Metadata about Media')
-        unique_together = (
-            ('media', 'site', 'key'),
-        )
-        get_latest_by = ["-retrieved", "-created"]
-
-    uuid = models.UUIDField(
-        _('uuid'),
-        primary_key=True,
-        editable=False,
-        default=uuid.uuid4,
-        help_text=_('UUID of the metadata'),
-    )
-    media = models.OneToOneField(
-        Media,
-        # on_delete=models.DO_NOTHING,
-        on_delete=models.SET_NULL,
-        related_name='new_metadata',
-        help_text=_('Media the metadata belongs to'),
-        null=True,
-        parent_link=False,
-    )
-    site = models.CharField(
-        _('site'),
-        max_length=256,
-        blank=True,
-        db_index=True,
-        null=False,
-        default='Youtube',
-        help_text=_('Site from which the metadata was retrieved'),
-    )
-    key = models.CharField(
-        _('key'),
-        max_length=256,
-        blank=True,
-        db_index=True,
-        null=False,
-        default='',
-        help_text=_('Media identifier at the site from which the metadata was retrieved'),
-    )
-    created = models.DateTimeField(
-        _('created'),
-        auto_now_add=True,
-        db_index=True,
-        help_text=_('Date and time the metadata was created'),
-    )
-    retrieved = models.DateTimeField(
-        _('retrieved'),
-        auto_now_add=True,
-        db_index=True,
-        help_text=_('Date and time the metadata was retrieved'),
-    )
-    uploaded = models.DateTimeField(
-        _('uploaded'),
-        db_index=True,
-        null=True,
-        help_text=_('Date and time the media was uploaded'),
-    )
-    published = models.DateTimeField(
-        _('published'),
-        db_index=True,
-        null=True,
-        help_text=_('Date and time the media was published'),
-    )
-    value = models.JSONField(
-        _('value'),
-        encoder=JSONEncoder,
-        null=False,
-        default=dict,
-        help_text=_('JSON metadata object'),
-    )
-
-
-    def __str__(self):
-        template = '"{}" from {} at: {}'
-        return template.format(
-            self.key,
-            self.site,
-            self.retrieved.isoformat(timespec='seconds'),
-        )
-
-    @atomic(durable=False)
-    def ingest_formats(self, formats=list(), /):
-        number = 0
-        for number, format in enumerate(formats, start=1):
-            mdf, created = self.format.get_or_create(site=self.site, key=self.key, number=number)
-            mdf.value = format
-            mdf.save()
-        if number > 0:
-            # delete any numbers we did not overwrite or create
-            self.format.filter(site=self.site, key=self.key, number__gt=number).delete()
-
-    @property
-    def with_formats(self):
-        formats = self.format.all().order_by('number')
-        formats_list = [ f.value for f in qs_gen(formats) ]
-        metadata = self.value.copy()
-        metadata.update(dict(formats=formats_list))
-        return metadata
-
-    @atomic(durable=False)
-    def ingest_metadata(self, data):
-        assert isinstance(data, dict), type(data)
-        from common.timestamp import timestamp_to_datetime
-
-        try:
-            self.retrieved = timestamp_to_datetime(
-                self.media.get_metadata_first_value(
-                    'epoch',
-                    arg_dict=data,
-                )
-            ) or self.created
-        except AssertionError:
-            self.retrieved = self.created
-
-        try:
-            self.published = timestamp_to_datetime(
-                self.media.get_metadata_first_value(
-                    ('release_timestamp', 'timestamp',),
-                    arg_dict=data,
-                )
-            ) or self.media.published
-        except AssertionError:
-            self.published = self.media.published
-
-        self.value = data.copy() # try not to have side-effects for the caller
-        formats_key = self.media.get_metadata_field('formats')
-        formats = self.value.pop(formats_key, list())
-        self.uploaded = min(
-            self.published,
-            self.retrieved,
-            self.media.created,
-        )
-        self.save()
-        self.ingest_formats(formats)
-
-        return self.with_formats
 
