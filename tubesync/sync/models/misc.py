951b24bbefddd8d31b8c90e90ce99b56f8a4da98
tcely <tcely@users.noreply.github.com>
2025-05-09 02:01:49 -0400

    Update misc.py

--- tubesync/sync/models/misc.py
+++ tubesync/sync/models/misc.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from ..choices import Val, YouTube_SourceType
@@ -5,4 +6,18 @@ from ..choices import Val, YouTube_SourceType
 
 media_file_storage = FileSystemStorage(location=str(settings.DOWNLOAD_ROOT), base_url='/media-data/')
 _srctype_dict = lambda n: dict(zip( YouTube_SourceType.values, (n,) * len(YouTube_SourceType.values) ))
+
+
+def get_media_file_path(instance, filename):
+    return instance.filepath
+
+
+def get_media_thumb_path(instance, filename):
+    # we don't want to use alternate names for thumb files
+    if instance.thumb:
+        instance.thumb.delete(save=False)
+    fileid = str(instance.uuid).lower()
+    filename = f'{fileid}.jpg'
+    prefix = fileid[:2]
+    return Path('thumbs') / prefix / filename
 
