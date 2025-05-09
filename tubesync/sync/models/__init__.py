971be3d3dd7c584cb1d03b32adddc287dee604d9
tcely <tcely@users.noreply.github.com>
2025-05-09 02:05:53 -0400

    Update __init__.py

--- tubesync/sync/models/__init__.py
+++ tubesync/sync/models/__init__.py
@@ -1,3 +1,5 @@
+from common.json import JSONEncoder
+
 from .media import Media
 from .source import Source
 from .metadata import Metadata
@@ -5,7 +7,7 @@ from .metadata_format import MetadataFormat
 from .media_server import MediaServer
 
 from .misc import (
-    get_media_file_path,
+    #get_media_file_path,
     get_media_thumb_path,
     media_file_storage,
 )
