3bb19861cd9295dbb7bca7cd0748d4c8a22a9f86
tcely <tcely@users.noreply.github.com>
2025-05-09 03:40:20 -0400

    Add `_nfo_element` function

--- tubesync/sync/models/misc.py
+++ tubesync/sync/models/misc.py
@@ -8,6 +8,13 @@ media_file_storage = FileSystemStorage(location=str(settings.DOWNLOAD_ROOT), bas
 _srctype_dict = lambda n: dict(zip( YouTube_SourceType.values, (n,) * len(YouTube_SourceType.values) ))
 
 
+def _nfo_element(nfo, label, text, /, *, attrs={}, tail='\n', char=' ', indent=2):
+    element = nfo.makeelement(label, attrs)
+    element.text = text
+    element.tail = tail + (char * indent)
+    return element
+
+
 def get_media_file_path(instance, filename):
     return instance.filepath
 
