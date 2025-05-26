73ee968bdd717b181ac0f1436028730a3c2bb001
tcely <tcely@users.noreply.github.com>
2025-05-26 06:46:31 -0400

    Update views.py

--- tubesync/sync/views.py
+++ tubesync/sync/views.py
@@ -277,15 +277,6 @@ class ValidateSourceView(FormView):
 class EditSourceMixin:
     model = Source
     form_class = SourceForm
-    # manual ordering
-    fields = ('source_type', 'key', 'name', 'directory', 'filter_text', 'filter_text_invert', 'filter_seconds', 'filter_seconds_min',
-              'media_format', 'target_schedule', 'index_schedule', 'index_videos', 'index_streams', 'download_media',
-              'download_cap', 'delete_old_media', 'days_to_keep', 'source_resolution', 'source_vcodec', 'source_acodec',
-              'prefer_60fps', 'prefer_hdr', 'fallback',
-              'delete_removed_media', 'delete_files_on_disk', 'copy_channel_images',
-              'copy_thumbnails', 'write_nfo', 'write_json', 'embed_metadata', 'embed_thumbnail',
-              'enable_sponsorblock', 'sponsorblock_categories', 'write_subtitles',
-              'auto_subtitles', 'sub_langs')
     errors = {
         'invalid_media_format': _('Invalid media format, the media format contains '
                                   'errors or is empty. Check the table at the end of '
