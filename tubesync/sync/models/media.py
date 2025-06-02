db7af44cec1b8a9059969748157f1222ffdaa270
tcely <tcely@users.noreply.github.com>
2025-06-02 15:03:32 -0400

    Adjust the `seconds_to_timestr` import in sync/media.py

--- tubesync/sync/models/media.py
+++ tubesync/sync/models/media.py
@@ -18,13 +18,14 @@ from common.errors import NoFormatException
 from common.json import JSONEncoder
 from common.utils import (
     clean_filename, clean_emoji, glob_quote,
+    seconds_to_timestr,
 )
 from ..youtube import (
     get_media_info as get_youtube_media_info,
     download_media as download_youtube_media,
 )
 from ..utils import (
-    seconds_to_timestr, parse_media_format, filter_response,
+    parse_media_format, filter_response,
     write_text_file, mkdir_p, multi_key_sort,
 )
 from ..matching import (
