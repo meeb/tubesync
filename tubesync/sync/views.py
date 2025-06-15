276eca0d2800e444ed61c2882fe02bad138fb7b7
tcely <tcely@users.noreply.github.com>
2025-06-15 13:16:23 -0400

    Don't create a repeating indexing task in the web UI

--- tubesync/sync/views.py
+++ tubesync/sync/views.py
@@ -999,14 +999,6 @@ class ResetTasks(FormView):
                 str(source.pk),
                 verbose_name=verbose_name.format(source.name),
             )
-            # Recreate the initial indexing task
-            verbose_name = _('Index media from source "{}"')
-            index_source_task(
-                str(source.pk),
-                repeat=source.index_schedule,
-                schedule=source.task_run_at_dt,
-                verbose_name=verbose_name.format(source.name)
-            )
             # This also chains down to call each Media objects .save() as well
             source.save()
         return super().form_valid(form)
