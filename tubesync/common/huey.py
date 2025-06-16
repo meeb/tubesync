5f4fc46586c12e9f20434386b4a05639b859f250
tcely <tcely@users.noreply.github.com>
2025-06-16 18:06:34 -0400

    Patching the class was easier

--- tubesync/common/huey.py
+++ tubesync/common/huey.py
@@ -1,9 +1,11 @@
 from functools import wraps
+from huey import CancelExecution
 
 
-def CancelExecution(*args, retry=None, **kwargs):
-    from huey import CancelExecution as cancel
-    return cancel(retry, *args, **kwargs)
+def CancelExecution_init(self, *args, retry=None, **kwargs):
+    self.retry = retry
+    super().__init__(*args, **kwargs)
+CancelExecution.__init__ = CancelExecution_init
 
 
 def delay_to_eta(delay, /):
