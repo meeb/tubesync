cc9364841edad5a49b5cedfb1856914f166231e8
tcely <tcely@users.noreply.github.com>
2025-06-29 14:02:22 -0400

    Update tasks.py

--- tubesync/common/models/tasks.py
+++ tubesync/common/models/tasks.py
@@ -19,6 +19,9 @@ from background_task.settings import app_settings
 from background_task.signals import task_failed
 from background_task.signals import task_rescheduled
 
+from ..json import JSONEncoder
+# from common.json import JSONEncoder
+
 '''
 logger = logging.getLogger(__name__)
 
@@ -123,6 +126,8 @@ class TaskHistoryQuerySet(models.QuerySet):
             failed_at__isnull=False,
         )
         if within:
+            if not isinstance(within, timedelta):
+                within = timedelta(seconds=within)
             time_limit = timezone.now() - within
             qs = qs.filter(failed_at__gt=time_limit)
         return qs
@@ -137,30 +142,30 @@ class TaskHistoryQuerySet(models.QuerySet):
             failed_at__isnull=True,
         )
         if within:
+            if not isinstance(within, timedelta):
+                within = timedelta(seconds=within)
             time_limit = timezone.now() - within
-            qs = qs.filter(run_at__gt=time_limit)
+            qs = qs.filter(end_at__gt=time_limit)
         return qs
 
 
 class TaskHistory(models.Model):
     # the "name" of the task/function to be run
     name = models.CharField(max_length=190, db_index=True)
+    task_id = models.models.CharField(max_length=40, db_index=True)
     # the json encoded parameters to pass to the task
-    task_params = models.TextField()
+    task_params = models.JSONField(default=dict, encoder=JSONEncoder)
 
     verbose_name = models.CharField(max_length=255, null=True, blank=True)
 
-    # what priority the task has
-    priority = models.IntegerField(default=0, db_index=True)
+    start_at = models.DateTimeField(db_index=True, null=True, blank=True)
     # when the task was completed
-    end_at = models.DateTimeField(db_index=True)
+    end_at = models.DateTimeField(default=timezone.now, db_index=True)
 
-    repeat = models.BigIntegerField(default=0)
-    repeat_until = models.DateTimeField(null=True, blank=True)
-
+    # what priority the task had
+    priority = models.IntegerField(default=0, db_index=True)
     # the "name" of the queue this is to be run on
-    queue = models.CharField(max_length=190, db_index=True,
-                             null=True, blank=True)
+    queue = models.CharField(max_length=190, db_index=True, null=True, blank=True)
 
     # how many times the task has been tried
     attempts = models.IntegerField(default=0, db_index=True)
@@ -169,21 +174,26 @@ class TaskHistory(models.Model):
     # details of the error that occurred
     last_error = models.TextField(blank=True)
 
-    locked_at = models.DateTimeField(db_index=True, null=True, blank=True)
+    repeat = models.BigIntegerField(default=0)
+    repeat_until = models.DateTimeField(null=True, blank=True)
 
     objects = TaskHistoryQuerySet.as_manager()
 
+    def save(self, *args, **kwargs):
+        self.queue = self.queue or None
+        self.verbose_name = self.verbose_name or None
+        return super().save(*args, **kwargs)
+        
     def has_error(self):
         """
         Check if the last_error field is empty.
         """
         return bool(self.last_error)
-    has_error.boolean = True
 
     def __str__(self):
         return u'{} - {}'.format(
             self.verbose_name or self.task_name,
-            self.run_at,
+            self.end_at,
         )
 
 
