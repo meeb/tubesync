4396283ce1882cd6b51a0b0f431f8c5836397327
tcely <tcely@users.noreply.github.com>
2025-07-13 01:49:55 -0400

    Add the new original function

--- patches/yt_dlp/patch/fatal_http_errors.py
+++ patches/yt_dlp/patch/fatal_http_errors.py
@@ -20,6 +20,23 @@ class PatchedYoutubeIE(YoutubeIE):
 
         return webpage, master_ytcfg, player_responses, player_url
 
+    def _download_initial_webpage(self, webpage_url, webpage_client, video_id):
+        webpage = None
+        if webpage_url and 'webpage' not in self._configuration_arg('player_skip'):
+            query = {'bpctr': '9999999999', 'has_verified': '1'}
+            pp = (
+                self._configuration_arg('player_params', [None], casesense=True)[0]
+                or traverse_obj(INNERTUBE_CLIENTS, (webpage_client, 'PLAYER_PARAMS', {str}))
+            )
+            if pp:
+                query['pp'] = pp
+            webpage = self._download_webpage_with_retries(
+                webpage_url, video_id, query=query,
+                headers=traverse_obj(self._get_default_ytcfg(webpage_client), {
+                    'User-Agent': ('INNERTUBE_CONTEXT', 'client', 'userAgent', {str}),
+                }))
+        return webpage
+
 
 YoutubeIE.__unpatched___download_player_responses = YoutubeIE._download_player_responses
 YoutubeIE._download_player_responses = PatchedYoutubeIE._download_player_responses
