771b7507ce6add1fd8c9022c45f2e2dd600c6a57
tcely <tcely@users.noreply.github.com>
2025-08-15 05:19:35 -0400

    Remove `background_task` from logger.py

--- tubesync/common/logger.py
+++ tubesync/common/logger.py
@@ -31,19 +31,3 @@ if (
             _level, _msg = logging.INFO, next(iter(_spec))
         app_logger.log(_level, _msg)
 
-class NoWaitingForTasksFilter(logging.Filter):
-    def filter(self, record):
-        return 'waiting for tasks' != record.getMessage()
-
-background_task_name = 'background_task.management.commands.process_tasks'
-last_part = background_task_name.rsplit('.', 1)[-1]
-background_task_formatter = logging.Formatter(
-    f'%(asctime)s [{last_part}/%(levelname)s] %(message)s'
-)
-background_task_sh = logging.StreamHandler()
-background_task_sh.addFilter(NoWaitingForTasksFilter())
-background_task_sh.setFormatter(background_task_formatter)
-background_task_sh.setLevel(logging_level)
-background_task_logger = logging.getLogger(background_task_name)
-background_task_logger.addHandler(background_task_sh)
-background_task_logger.setLevel(logging_level)
