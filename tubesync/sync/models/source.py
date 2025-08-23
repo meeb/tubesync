7bebc96dc332c5e594df9d894e6ecb8ab61dc571
tcely <tcely@users.noreply.github.com>
2025-08-23 03:59:44 -0400

    Call `choices_bool`

--- tubesync/sync/models/source.py
+++ tubesync/sync/models/source.py
@@ -194,7 +194,7 @@ class Source(db.models.Model):
     )
     filter_seconds_min = db.models.BooleanField(
         _('filter seconds min/max'),
-        choices=FilterSeconds.choices_bool,
+        choices=FilterSeconds.choices_bool(),
         default=Val(FilterSeconds.MIN),
         help_text=_(
             'When Filter Seconds is > 0, do we skip on minimum (video shorter than limit) or maximum (video '
