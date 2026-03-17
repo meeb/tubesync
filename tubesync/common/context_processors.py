caa31ad8bd3306183b48ffcaa09671f8e6f39e08
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-17 07:55:22 +0000

    Improve Python file tubesync\common\context_processors.py

--- tubesync/common/context_processors.py
+++ tubesync/common/context_processors.py
@@ -1,11 +1,15 @@
 from django.conf import settings
-from .third_party_versions import yt_dlp_version, ffmpeg_version, deno_version
 
+def app_details(request: object) -> dict:
+    """
+    Returns a dictionary containing application and third-party library versions.
 
-def app_details(request):
+    :param request: The current request object (not used in this function)
+    :return: A dictionary with application and third-party library versions
+    """
     return {
-        'app_version': str(settings.VERSION),
-        'yt_dlp_version': yt_dlp_version,
-        'ffmpeg_version': ffmpeg_version,
-        'deno_version': deno_version,
+        'app_version': str(settings.VERSION),  # Convert VERSION to string
+        'yt_dlp_version': yt_dlp_version,  # Imported from .third_party_versions
+        'ffmpeg_version': ffmpeg_version,  # Imported from .third_party_versions
+        'deno_version': deno_version,  # Imported from .third_party_versions
     }
