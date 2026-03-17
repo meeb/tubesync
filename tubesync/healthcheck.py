f4c7341f89573442b1ccbf84c29b2e74ea265d68
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-17 07:54:57 +0000

    Improve Python file tubesync\healthcheck.py

--- tubesync/healthcheck.py
+++ tubesync/healthcheck.py
@@ -1,46 +1,61 @@
 #!/usr/bin/python3
-'''
 
-    Perform an HTTP request to a URL and exit with an exit code of 1 if the
-    request did not return an HTTP/200 status code.
+"""
+Perform an HTTP request to a URL and exit with an exit code of 1 if the request
+did not return an HTTP/200 status code.
 
-    Usage:
-    $ ./healthcheck.py http://some.url.here/healthcheck/resource
-
-'''
-
+Usage:
+$ ./healthcheck.py http://some.url.here/healthcheck/resource
+"""
 
 import os
 import sys
 import requests
 
+# Timeout in seconds for HTTP requests
+TIMEOUT: int = 5
 
-TIMEOUT = 5  # Seconds
-HTTP_USER = os.getenv('HTTP_USER')
-HTTP_PASS = os.getenv('HTTP_PASS')
-# never use proxy for healthcheck requests
+# HTTP authentication credentials from environment variables
+HTTP_USER: str = os.getenv('HTTP_USER')
+HTTP_PASS: str = os.getenv('HTTP_PASS')
+
+# Disable proxy for healthcheck requests
 os.environ['no_proxy'] = '*'
 
 
-def do_heatlhcheck(url):
+def do_healthcheck(url: str) -> bool:
+    """
+    Perform a GET request to the specified URL and return True if the response
+    status code is 200, False otherwise.
+
+    :param url: URL to perform the healthcheck on
+    :return: True if the healthcheck was successful, False otherwise
+    """
     headers = {'User-Agent': 'healthcheck'}
     auth = None
     if HTTP_USER and HTTP_PASS:
         auth = (HTTP_USER, HTTP_PASS)
-    response = requests.get(url, headers=headers, auth=auth, timeout=TIMEOUT)
-    return response.status_code == 200
+    try:
+        response = requests.get(url, headers=headers, auth=auth, timeout=TIMEOUT)
+        response.raise_for_status()  # Raise an exception for bad status codes
+        return True
+    except requests.RequestException as e:
+        print(f"Error performing healthcheck: {e}", file=sys.stderr)
+        return False
 
 
 if __name__ == '__main__':
-    # if it is marked as intentionally down, nothing else matters
+    # If the service is intentionally down, exit immediately
     if os.path.exists('/run/service/gunicorn/down'):
         sys.exit(0)
-    try:
-        url = sys.argv[1]
-    except IndexError:
-        sys.stderr.write('URL must be supplied\n')
+
+    # Check if a URL was provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("URL must be supplied", file=sys.stderr)
         sys.exit(1)
-    if do_heatlhcheck(url):
+
+    url = sys.argv[1]
+    if do_healthcheck(url):
         sys.exit(0)
     else:
         sys.exit(1)
