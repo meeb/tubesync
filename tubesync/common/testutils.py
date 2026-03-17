48c7e596caf05f8eec579b7e2bd49717afb0e340
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-17 07:56:05 +0000

    Improve Python file tubesync\common\testutils.py

--- tubesync/common/testutils.py
+++ tubesync/common/testutils.py
@@ -1,19 +1,30 @@
 import logging
 
+def prevent_request_warnings(original_function: callable) -> callable:
+    """
+    Suppresses errors from views that raise legitimate errors, such as testing
+    that a page does indeed 404 or a non-authenticated user cannot access a
+    page requiring authentication which raises a 403.
 
-def prevent_request_warnings(original_function):
-    '''
-        Suppress errors from views that raise legitimate errors, such as
-        testing that a page does indeed 404 or a non-authenticated user
-        cannot access page requiring authentication which raises a 403. You
-        can wrap test methods with this to drop the error logging down a notch.
-    '''
-    
-    def new_function(*args, **kwargs):
+    Args:
+        original_function: The function to be wrapped.
+
+    Returns:
+        A new function that suppresses request warnings.
+    """
+    def new_function(*args: tuple, **kwargs: dict) -> None:
+        # Get the current logger and store its level
         logger = logging.getLogger('django.request')
         previous_logging_level = logger.getEffectiveLevel()
+
+        # Temporarily set the logger level to CRITICAL to suppress warnings
         logger.setLevel(logging.CRITICAL)
-        original_function(*args, **kwargs)
-        logger.setLevel(previous_logging_level)
+
+        try:
+            # Call the original function with the provided arguments
+            original_function(*args, **kwargs)
+        finally:
+            # Restore the original logger level
+            logger.setLevel(previous_logging_level)
 
     return new_function
