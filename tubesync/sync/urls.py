7a92aa704fe94a92837a10da348ce5a5153fd468
tcely <tcely@users.noreply.github.com>
2026-04-01 04:01:34 -0400

    Remove the duplicated import

--- tubesync/sync/urls.py
+++ tubesync/sync/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (DashboardView, SourcesView, SourceSyncNowView,
+from .views import (DashboardView, SourcesView,
                     ValidateSourceView, AddSourceView,
                     SourceView, UpdateSourceView, DeleteSourceView, MediaView,
                     MediaThumbView, MediaItemView, MediaRedownloadView, MediaSkipView,
