328c2649bd9b306a970a1497af976c0c3cd4209a
tcely <tcely@users.noreply.github.com>
2026-04-24 23:45:41 -0400

    chore: migrate `download_asfald` to a file

--- tubesync/install_tailwindcss.sh
+++ tubesync/install_tailwindcss.sh
@@ -8,6 +8,7 @@ set -eu
 HERE="$(dirname "$(realpath "$0")")"
 source "${HERE}/download_gh_release.func.inc.sh"
 source "${HERE}/verify_digest.func.inc.sh"
+source "${HERE}/download_asfald.func.inc.sh"
 
 bin_directory() {
     local dir="${1:-/usr/local/bin}"
@@ -18,42 +19,6 @@ bin_directory() {
     else
         printf -- '%s\n' "$(dirname "${dir}")"
     fi
-}
-
-download_asfald() {
-    local owner='asfaload' repo='asfald' tag='v0.6.0'
-    local asfald_uri="${owner}/${repo}/releases/download/${tag}/checksums.txt"
-    local sums_url="https://gh.checksums.asfaload.com/github.com/${asfald_uri}"
-
-    local os
-    case "$(uname -s)" in
-        (Darwin) os='apple-darwin' ;;
-        (Linux) os='unknown-linux-musl' ;;
-    esac
-    local arch
-    case "$(uname -m)" in
-        (aarch64|arm64) arch='aarch64' ;;
-        (x86_64) arch='x86_64' ;;
-    esac
-
-    case "${1-}" in
-        (latest)
-            TMPDIR="$(realpath .)" \
-                ./asfald -o 'asfald-latest' -w -p '${path}/checksums.txt' -- "https://github.com/${owner}/${repo}/releases/latest/download/asfald-${arch}-${os}" && \
-                chmod -v a+rx asfald-latest
-            local latest_version="$(./asfald-latest --version)"
-            test -n "${latest_version}" || return 1
-            local latest_digest="$(./asfald-latest --get-hash "https://github.com/${owner}/${repo}/releases/download/v${latest_version#asfald }/asfald-${arch}-${os}")"
-            verify_digest "${latest_digest}" 'asfald-latest' || return 1
-            ;;
-        (*)
-            download_gh_release "${owner}" "${repo}" "asfald-${arch}-${os}" "${tag}" && \
-                curl -sSL -- "${sums_url}" | "${HERE}/shasum.py" -a sha256 - && \
-                chmod -v a+rx "asfald-${arch}-${os}" && \
-                mv -v "asfald-${arch}-${os}" asfald
-            ;;
-    esac
-
 }
 
 download_tailwindcss() {
