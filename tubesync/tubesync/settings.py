ec2e8eabe68a94003496db41e4eb19c63dab4085
tcely <tcely@users.noreply.github.com>
2026-04-27 18:32:00 -0400

    fix: rename the middleware class

--- tubesync/tubesync/settings.py
+++ tubesync/tubesync/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'common.middleware.BulmaDefaultFieldsMiddleware',
+    'common.middleware.FormsDefaultFieldsMiddleware',
     'common.middleware.BasicAuthMiddleware',
 ]
 
