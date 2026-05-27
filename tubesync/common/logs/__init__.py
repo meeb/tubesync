4dd0b16bfcba64746f308ac38a9cd4d6a36558c8
tcely <tcely@users.noreply.github.com>
2026-05-27 05:39:42 -0400

    chore(logs): provide a default app_logger

--- tubesync/common/logs/__init__.py
+++ tubesync/common/logs/__init__.py
@@ -1,10 +1,16 @@
 from . import syslog
 from ._default import default_formatter, default_handler
 from ._filters import RemoveSpecificLogFilter
+from ._logger import app_logger, logger
+
+
+logger = logger(__name__)
 
 __all__ = [
+    'app_logger',
     'default_formatter',
     'default_handler',
+    'logger',
     'syslog',
     'RemoveSpecificLogFilter',
 ]
--- tubesync/common/logs/syslog/hat/_logger.py
+++ tubesync/common/logs/syslog/hat/_logger.py
@@ -1,5 +1,6 @@
 import logging
 
+
 logger = lambda name=None: logging.getLogger(
     __name__.rsplit('.', 1)[0] if name is None else name
 )
