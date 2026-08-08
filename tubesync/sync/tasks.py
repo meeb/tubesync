7f9eee8d8bd11269c6889e4d029170e4a2594630
tcely <tcely@users.noreply.github.com>
2026-08-08 08:48:20 -0400

    chore(tasks): switch over `download_source_images`

--- tubesync/sync/tasks.py
+++ tubesync/sync/tasks.py
@@ -709,7 +709,7 @@ def check_source_directory_exists(source_id):
         source.make_directory()
 
 
-@dynamic_retry(db_task, delay=10, priority=90, retries=15, queue=Val(TaskQueue.NET))
+@db_task(delay=10, priority=90, retries=15, backoff_class=DjangoBackgroundTasksBackoff, task_base=AttemptsTask, queue=Val(TaskQueue.NET))
 def download_source_images(source_id):
     '''
         Downloads an image and save it as a local thumbnail attached to a
