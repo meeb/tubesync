7893190350d5254b670e5a45373105f8518ac52d
tcely <tcely@users.noreply.github.com>
2026-09-11 10:31:07 -0400

    chore(migrations): list -> tuple

--- tubesync/sync/migrations/0032_alter_metadata_options_alter_metadataformat_options_and_more.py
+++ tubesync/sync/migrations/0032_alter_metadata_options_alter_metadataformat_options_and_more.py
@@ -13,11 +13,11 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name='metadata',
-            options={'get_latest_by': ['-retrieved', '-created'], 'verbose_name': 'Metadata about Media', 'verbose_name_plural': 'Metadata about Media'},
+            options={'get_latest_by': ('-retrieved', '-created'), 'verbose_name': 'Metadata about Media', 'verbose_name_plural': 'Metadata about Media'},
         ),
         migrations.AlterModelOptions(
             name='metadataformat',
-            options={'ordering': ['site', 'key', 'number'], 'verbose_name': 'Format from Media Metadata', 'verbose_name_plural': 'Formats from Media Metadata'},
+            options={'ordering': ('site', 'key', 'number'), 'verbose_name': 'Format from Media Metadata', 'verbose_name_plural': 'Formats from Media Metadata'},
         ),
         migrations.AlterUniqueTogether(
             name='metadataformat',
