2fdc27d748c5eeed0f61cd5ebffaadec51f01503
tcely <tcely@users.noreply.github.com>
2026-09-11 06:46:07 -0400

    fix(lint): address INT001

--- tubesync/sync/management/commands/fix-mariadb.py
+++ tubesync/sync/management/commands/fix-mariadb.py
@@ -141,9 +141,9 @@ class Command(BaseCommand):
 
         if options['uuid_columns']:
             if 'uuid' != db.connection.data_types.get('UUIDField', ''):
-                raise CommandError(_(
-                    f'The {display_name} database server does not support UUID columns.'
-                ))
+                raise CommandError(
+                    _('The %s database server does not support UUID columns.') % _(display_name),
+                )
             uuid_column_type_str = 'uuid(36)'
             both_tables = (
                 self._using_char('sync_source', 'uuid') and
