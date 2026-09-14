5de9352a7ed9432050843685217c4dc96afd2814
tcely <tcely@users.noreply.github.com>
2026-09-14 13:31:43 -0400

    fixup! fix: have curl retry

--- tubesync/download_gh_release.func.inc.sh
+++ tubesync/download_gh_release.func.inc.sh
@@ -55,5 +55,5 @@ download_gh_release() {
 
     stdout "Fetching from ${owner}/${repo}: ${filename}"
 
-    curl --progress-bar --fail --location --remote-name --remote-time --retries 5 --retry-all-errors "${dl_url}"
+    curl --progress-bar --fail --location --remote-name --remote-time --retry 5 --retry-connrefused --retry-all-errors "${dl_url}"
 }
