8fd16d44b99afcd899d49215a23f4d007603cae9
tcely <tcely@users.noreply.github.com>
2026-09-14 19:07:40 -0400

    fix(lint): logging warn -> warning
    Co-authored-by: tcely <tcely@users.noreply.github.com>

--- tubesync/sync/hooks.py
+++ tubesync/sync/hooks.py
@@ -127,7 +127,7 @@ class PPHookStatus(BaseStatus):
 
 def yt_dlp_progress_hook(event):
     if not ProgressHookStatus.valid_status(event['status']):
-        log.warn(f'[youtube-dl] unknown progress event: {event!s}')
+        log.warning(f'[youtube-dl] unknown progress event: {event!s}')
         return
 
     key = None
@@ -190,7 +190,7 @@ def yt_dlp_progress_hook(event):
 
 def yt_dlp_postprocessor_hook(event):
     if not PPHookStatus.valid_status(event['status']):
-        log.warn(f'[youtube-dl] unknown postprocessor event: {event!s}')
+        log.warning(f'[youtube-dl] unknown postprocessor event: {event!s}')
         return
 
     name = key = 'Unknown'
