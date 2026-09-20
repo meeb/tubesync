2976303b0cb427fb20edb3d01aeccb1531582941
tcely <tcely@users.noreply.github.com>
2026-09-20 09:51:37 -0400

    Decrease retry attempts for bun verification
    Reduce the number of attempts for bun verification from 5 to 3.

--- tubesync/install_bun.sh
+++ tubesync/install_bun.sh
@@ -72,8 +72,7 @@ extract_bun() {
     unzip -u -o -d './.bun' "${fn}" &&
         install -v -T ./.bun/bun-linux-*/bun "${_staged}" &&
         { # bun spawning unzip hangs for an unknown reason fairly often
-            "${_staged}" run "${HERE}/diagnose_bun_child_process.ts" || : ;
-            local _attempt ; for _attempt in {1..5} ; do
+            local _attempt ; for _attempt in {1..3} ; do
                 "${_staged}" run "${HERE}/verify_bun.ts" --install-dir "${dest_dir}" --release 'bun-v1.3.14' &&
                     break || sleep "${_attempt}"
             done ;
