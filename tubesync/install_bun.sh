1a7bbd3c6e172fb30148f89c1d47dbc97deed7f5
tcely <tcely@users.noreply.github.com>
2026-09-24 14:35:00 -0400

    chore(bun): stop forcing the error exit value

--- tubesync/install_bun.sh
+++ tubesync/install_bun.sh
@@ -72,13 +72,9 @@ extract_bun() {
     unzip -u -o -d './.bun' "${fn}" &&
         install -v -T ./.bun/bun-linux-*/bun "${_staged}" &&
         { # bun spawning unzip hangs for an unknown reason fairly often
-            local _force=1 ; 
             local _attempt ; for _attempt in {1..5} ; do
-                if [[ 4 < "${_attempt}" ]]; then
-                    _force=0
-                fi
-                INSTALL_BUN_FORCE_ERROR="${_force}" \
                 INSTALL_BUN_ATTEMPT="${_attempt}" \
+                INSTALL_BUN_FORCE_ERROR=0 \
                 "${_staged}" run "${HERE}/verify_bun.ts" --release 'bun-v1.3.14' --asset "${fn}" --install-dir "${dest_dir}" &&
                     break || sleep "${_attempt}"
             done ;
